17687ba15132d67f9fbac18440a81df2c4af1614
Youssef Hemimy <itsuzef@gmail.com>
2022-01-26 16:44:45 -0500

    NeuralNetwork

--- ConvolutionalNeuralNetwork.py
+++ ConvolutionalNeuralNetwork.py
@@ -1,104 +1,76 @@
 """""
-Creating a neural network using Keras. This model is testing the image classification
-accuracy. 
-It asks the user to input a number (1 to 1000), it shows the guess and the expected classification
+a convolutional neural network(CNN, or ConvNet) is a class of artificial neural network, 
+most commonly applied to analyze visual imagery.
+They are also known as shift invariant or space invariant artificial neural networks (SIANN), 
+based on the shared-weight architecture of the convolution kernels or filters that slide along input features and 
+provide translation equivariant responses known as feature maps.Counter-intuitively, most convolutional neural networks are only equivariant, 
+as opposed to invariant, to translation.
+They have applications in image and video recognition, recommender systems, image classification, image segmentation, 
+medical image analysis, natural language processing, brain-computer interfaces,and financial time series 
 
+Dense neural networks analyze input on a global scale and recognize patterns in specific areas. 
+Convolutional neural networks scan through the entire input a little at a time and learn local patterns.
 
+It outputs a response map, quantifying the presence of the filter's pattern at different locations 
+
+Three main properties of each convolutional layer: 
+Input size
+the number of filters
+the sample size of the filters.
 """
 
-from re import L
 import tensorflow as tf
-from tensorflow import keras
-import numpy as np
+from tensorflow.keras import datasets, layers, models
 import matplotlib.pyplot as plt
-
-#load fashion_mnist dataset 
-fashion_mnist = keras.datasets.fashion_mnist 
-
+ 
+#load cifar10 dataset 
 #split into testing and training
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-train_images.shape
-train_images[0,23,23]
-train_labels[:10]
-class_names = ['T-shirt/top','Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker','Bag', 'Ankle boot']
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+#normalize pixel values to be between 0 and 1
+train_images, test_images =train_images / 255.0 , test_images / 255.0
 
-plt.figure()
-plt.imshow(train_images[5])
-plt.colorbar()
-plt.grid(False)
+class_names = ['airplane','automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse','ship', 'truck']
+
+#looking at images, change the value 1 
+IMG_INDEX = 0
+plt.imshow(train_images[IMG_INDEX], cmap=plt.cm.binary)
+plt.xlabel(class_names[train_labels[IMG_INDEX][0]])
 plt.show()
 
-#Dats processing 
+#CNN Architecture 
+# a stack of Conv2D and MaxPooling2D layers followed by a densly connected layer. 
+# Conv2D and MaxPooling2D layers extract the features from the image 
+# These features are then flattened and fed to densly connected layers that determine the class of an image based on the presence of features. 
 
-train_images = train_images / 255.0 
+#building the Convolutional base 
 
-test_images = test_images / 255.0
+model = models.Sequential()
 
-#Building the model 
-model = keras.Sequential([
-    #input layer 
-    keras.layers.Flatten(input_shape=(28,28)), 
-    #hidden layer 
-    keras.layers.Dense(128, activation = 'relu'), 
-    #output layer 
-    keras.layers.Dense(10, activation = 'softmax')
-])
+#define amount of filters, size of filters, the activation function, the input shape 
+model.add(layers.Conv2D(32, (3, 3), activation= 'relu', input_shape=(32,32,3)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64, (3,3), activation = 'relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64, (3, 3), activation = 'relu'))
 
-#Compiling the model 
+# to take a look at the model 
+model.summary()
 
-model.compile(optimizer = 'adam', 
-              loss = 'sparse_categorical_crossentropy', 
-              metrics = ['accuracy'])
+#Adding Dense layers to classify the extracted features
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation = 'relu'))
+model.add(layers.Dense(10))
 
-#Train the model 
-model.fit(train_images, train_labels, epochs = 3)
+model.summary()
 
-#evaluate/test the model 
+# Training using the recommended hyper parameters from tensorflow
+model.compile(optimizer='adam', 
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
 
-test_loss, test_acc = model.evaluate(test_images, test_labels, verbose = 1)
-print('Test accuracy:', test_acc)
+history = model.fit(train_images, train_labels, epochs=10, 
+                    validation_data=(test_images, test_labels))
 
-predictions = model.predict(test_images)
-print(class_names[np.argmax(predictions[3])])
-test_images.shape
-plt.figure()
-plt.imshow(test_images[0])
-plt.colorbar()
-plt.grid(False)
-plt.show()
+#evaluating the model
 
-COLOR = 'white'
-plt.rcParams['text.color'] = COLOR
-plt.rcParams['axes.labelcolor'] = COLOR
-
-def predict(model,image,correct_label):
-    class_names = ['T-shirt/top','Trouser', 'Pullover', 'Dress', 'Coat', 
-                    'Sandal', 'Shirt', 'Sneaker','Bag', 'Ankle boot']
-    prediction = model.predict(np.array([image]))
-    predicted_class = class_names[np.argmax(prediction)]
-
-    show_image(image, class_names[correct_label], predicted_class)
-
-
-def show_image(img, label, guess):
-    plt.figure()
-    plt.imshow(img, cmap=plt.cm.binary)
-    plt.title("Expected: " + label)
-    plt.xlabel("Guess: " + guess)
-    plt.colorbar()
-    plt.grid(False)
-    plt.show()
-
-def get_number(): 
-    while True: 
-        num = input("Pick a number: ")
-        if num.isdigit(): 
-            num = int(num)
-            if 0 <= num <= 1000: 
-                return int(num)
-        else: 
-            print("try again.. ")
-num = get_number()
-image = test_images[num]
-label = test_labels[num]
-predict(model, image, label)
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=1)
